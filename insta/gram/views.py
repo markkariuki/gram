8ed75d696bfa715b033b40d1207f60057fbc576b
david mark <markkariuki17@gmail.com>
2018-01-29 14:43:20 +0300

    add change to the views and the models

--- insta/gram/views.py
+++ insta/gram/views.py
@@ -1,7 +1,8 @@
+from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as db
 
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to my gram which is being maintained will be back shortly')
+    return render(request, 'home.html')
