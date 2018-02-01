94d6383fe1cb429d61bd293aa8585c71305c85b2
david mark <markkariuki17@gmail.com>
2018-02-01 12:04:45 +0300

    add chnage to the models file

--- insta/gram/views.py
+++ insta/gram/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+ from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as db
 
@@ -6,3 +6,7 @@ import datetime as db
 # Create your views here.
 def welcome(request):
     return render(request, 'home.html')
+
+def signup(request):
+    context = {}
+    return render(request, 'sign-up.html', context)    
